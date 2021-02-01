da868412e953afa03c332cbfda7057155665931f
Laurence Ion <laurion@users.noreply.github.com>
2021-02-01 13:10:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let turns = 0;
 let money = 250;
 let headsProbability = 0.6;
-var firebaseConfig = {
-  apiKey: "AIzaSyBxUpM2HJJwkfICQmsuYfonGxklDh5sfP4",
-  authDomain: "super-coin-13eab.firebaseapp.com",
-  projectId: "super-coin-13eab",
-  storageBucket: "super-coin-13eab.appspot.com",
-  messagingSenderId: "99550835437",
-  appId: "1:99550835437:web:f911c6e27dfe96443852e4",
-  measurementId: "G-GR31E5D3TC"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-const db = firebase.database();
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBxUpM2HJJwkfICQmsuYfonGxklDh5sfP4",
+//   authDomain: "super-coin-13eab.firebaseapp.com",
+//   projectId: "super-coin-13eab",
+//   storageBucket: "super-coin-13eab.appspot.com",
+//   messagingSenderId: "99550835437",
+//   appId: "1:99550835437:web:f911c6e27dfe96443852e4",
+//   measurementId: "G-GR31E5D3TC"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+// const db = firebase.database();
 document.getElementById("flip_button").onclick = (e) => { 
   console.log("@");
   
@@ -39,6 +39,6 @@ document.getElementById("flip_button").onclick = (e) => {
   row.insertCell().innerHTML = betAmount;
   row.insertCell().innerHTML = betResult ? "Win" : "Loss";
   row.insertCell().innerHTML = money;
-  db.ref('bets').push({turns,headsChecked,betAmount,betResult,money})
+  // db.ref('bets').push({turns,headsChecked,betAmount,betResult,money})
   return false;
 }
